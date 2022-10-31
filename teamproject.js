28c84037e3330c766e3a9c70d1ad1a7ecb4131fd
rudqhd79 <rudqhd79@naver.com>
2022-10-31 23:04:08 +0900

    2022.10.31: at home

--- teamproject.js
+++ teamproject.js
@@ -1,39 +1,12 @@
-// const one = document.querySelector('#one');
-// const two = document.querySelector("#two");
-// const three = document.querySelector('#three');
-// const four = document.querySelector("#four");
-// const five = document.querySelector("#five");
-// const six = document.querySelector("#six");
-// const seven = document.querySelector("#seven");
-// const eight = document.querySelector("#eight");
-// const nine = document.querySelector("#nine");
-// const ten = document.querySelector("#ten");
-// const tenone = document.querySelector("#tenone");
-// const tentwo = document.querySelector("#tentwo");
-// const tenthree = document.querySelector("#tenthree");
-// const tenfour = document.querySelector("#tenfour");
-// const tenfive = document.querySelector("#tenfive");
+const tit2 = document.querySelector('.tit2');
+const toggle = document.querySelector(".toggle");
 
+toggle.addEventListener('click', () => {
+  const dropdown = document.querySelector(".dropdown");
+  dropdown.style.display = 'block';
+});
 
-document.addEventListener('click', function (event) {
-  
-  if (!event.target.classList.contains('toggle')) return;
-  
-  var content = document.querySelector(event.target.hash);
-  if (!content) return;
-  
-  event.preventDefault();
-  
-  if (content.classList.contains('.active')) {
-    content.classList.remove('active');
-    return;
-  }
-  
-  var accordions = document.querySelectorAll('.tit2');
-  for (var i = 0; i < accordions.length; i++) {
-    accordions[i].classList.remove('active');
-  }
-  
-  content.classList.toggle('active');
+toggle.addEventListener('blur', () => {
+  const dropdown = document.querySelector(".dropdown");
+  dropdown.style.display = '';
 })
-
