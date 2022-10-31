dc7d034617c41562c0f475e6f1bb8e1c1e070b33
rudqhd79 <rudqhd79@naver.com>
2022-10-31 20:30:55 +0900

    2022.10.31 최종

--- teamproject.js
+++ teamproject.js
@@ -1,31 +1,39 @@
-const one = document.querySelector('#one');
-const two = document.querySelector("#two");
-const three = document.querySelector('#three');
-const four = document.querySelector("#four");
-const five = document.querySelector("#five");
-const six = document.querySelector("#six");
-const seven = document.querySelector("#seven");
-const eight = document.querySelector("#eight");
-const nine = document.querySelector("#nine");
-const ten = document.querySelector("#ten");
-const tenone = document.querySelector("#tenone");
-const tentwo = document.querySelector("#tentwo");
-const tenthree = document.querySelector("#tenthree");
-const tenfour = document.querySelector("#tenfour");
-const tenfive = document.querySelector("#tenfive");
+// const one = document.querySelector('#one');
+// const two = document.querySelector("#two");
+// const three = document.querySelector('#three');
+// const four = document.querySelector("#four");
+// const five = document.querySelector("#five");
+// const six = document.querySelector("#six");
+// const seven = document.querySelector("#seven");
+// const eight = document.querySelector("#eight");
+// const nine = document.querySelector("#nine");
+// const ten = document.querySelector("#ten");
+// const tenone = document.querySelector("#tenone");
+// const tentwo = document.querySelector("#tentwo");
+// const tenthree = document.querySelector("#tenthree");
+// const tenfour = document.querySelector("#tenfour");
+// const tenfive = document.querySelector("#tenfive");
 
 
-function dropdown() {
-    document.getElementById("one").classList.toggle("show");
-}
-if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("list_1");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+document.addEventListener('click', function (event) {
+  
+  if (!event.target.classList.contains('toggle')) return;
+  
+  var content = document.querySelector(event.target.hash);
+  if (!content) return;
+  
+  event.preventDefault();
+  
+  if (content.classList.contains('.active')) {
+    content.classList.remove('active');
+    return;
   }
+  
+  var accordions = document.querySelectorAll('.tit2');
+  for (var i = 0; i < accordions.length; i++) {
+    accordions[i].classList.remove('active');
+  }
+  
+  content.classList.toggle('active');
+})
+
